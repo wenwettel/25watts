b7477d8a070c9a8831e9d083724db783117dd5e8
Wendy Wettel <wenwettel@gmail.com>
2020-12-12 13:25:12 -0300

    responsive

---
+++ js/index.js
@@ -0,0 +1,41 @@
+//Validacion de Formulario
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
+
+// scroll nav
+
+var nav = document.getElementById("navegation");
+console.log(nav);
+window.addEventListener("scroll", function (e) {
+  console.log(window.pageYOffset);
+  var scrollPos = window.pageYOffset;
+
+  if (scrollPos !== 0) {
+    nav.classList.add("scroll");
+  } else {
+    nav.classList.remove("scroll");
+  }
+});
